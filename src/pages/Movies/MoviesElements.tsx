9208ef5914fe8296afc3bb3b476c7a7080e5f6f1
Oddie <romualdo.gui@gmail.com>
2021-05-25 08:28:05 -0300

    Create Movie page, add some elements

---
+++ src/pages/Movies/MoviesElements.tsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+
+export const Select = styled.select`
+display: flex;
+background-color: gray;
+border-radius: 5px;
+color: red;
+background: black;
+margin-top: 5%;
+margin-left: 150px;
+width: 165px;
+height: 44px;
+font-family: system-ui;
+font-weight: bold;
+ option {
+    font-family: system-ui;
+font-weight: bold;
+color: red;
+ }
+`
+
+export const ContainerSelect = styled.div`
+display: flex;
+background-color: gray;
+width: 100%;
+height: 100%;
+margin: auto;
+
+`
--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavBar from '../../components/NavBar';
+import { ContainerSelect, Select } from './MoviesElements';
+import MovieInformation from '../../components/MovieInformation';
+import { ContainerCards, ContainerButton, BackButton, Button, ContainerOpacity, ContainerAbsolute } from '../Personages/PersonagesElements';
+
+interface MoviesValues {
+  selectedValue: string;
+  
+}
 
 
-const Movies: React.FC = () => {
+const Movies: React.FC<MoviesValues> = () => {
+  const [value, setValue] = useState<string>("");
+  var display = false;
+  const handleChange = (event: any) => {
+    setValue(event.target.value);
+    console.log(value)
+  };
+  
   return <>
-  
-  
+    <NavBar></NavBar>
+    <ContainerSelect>
+      <Select id="list" onChange={handleChange}>
+        <option value="" selected disabled hidden >Filtrar por:</option>
+        <option value="Lançamento">Lançamento</option>
+        <option value="Cronologia">Cronologia</option>
+      </Select>
+    </ContainerSelect> 
+
   </>;
 }
 
